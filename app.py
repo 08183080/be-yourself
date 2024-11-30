5fa5ca76b4d3cb06edc5ad0bef8b3699fc8eec93
08183080 <08183080@cumt.edu.cn>
2024-11-30 22:53:11 +0800

    demo 来了

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+import yagmail
+import schedule
+import time
+import random
+
+def get_contents(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+def get_emails(path):
+    with open(path, 'r') as f:
+        return f.read().splitlines()
+
+
+def get_one_file(path):
+    '''
+    在path目录中随便选择个文件，返回路径+文件名的绝对路径
+    '''
+    files = os.listdir(path)
+    return path + '\\' + random.choice(files)
+
+def send_email(src, dst, subject, contents, attachments):
+    pwd = os.environ.get('wangyi_emai_auth')
+
+    yag = yagmail.SMTP(user=src, password=pwd, host='smtp.163.com', port='465')
+    yag.send(to=dst, subject=subject, contents=contents, attachments=attachments)
+    yag.close()
+
+def send_emails(src, tos, subject, contents, attachments):
+    for to in tos:
+        send_email(src, to, subject, contents, attachments)  
+
+def daily_task():
+    try:
+        p = r'D:\be-yourself\data'
+        path = get_one_file(p)
+        src = '19121220286@163.com'
+        tos = get_emails('emails.txt') 
+        subject = '今日戒色信息流'
+        contents = get_contents(path)
+        attachments = path
+        
+        send_emails(src, tos, subject, contents, attachments)
+    except Exception as e:
+        print(f"{e} occured in daily_task")
+
+if __name__ == '__main__':
+    try:
+        schedule.every().day.at('19:00').do(daily_task)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    except Exception as e:
+        print(f"{e} occured~")
---
+++ utils/get_many.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1'
+}
+
+
+def get_one(url):
+    response = requests.get(url, headers)
+    if response.status_code == 200:
+        text = response.text
+        soup = BeautifulSoup(text, 'html.parser')
+        title = soup.find('h1', class_='entry-title').text
+        content = soup.find('div', class_='single-content').text
+
+        return title, content
+
+def write_one(title, content, path):
+    with open(path + '\\'+ title + '.txt', 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
+if __name__ == '__main__':
+    for i in range(42602, 42800):
+        url = f'https://www.jiexy.com/{i}.html'
+        try:
+            title, content = get_one(url)
+            print(title)
+            print(content)
+            write_one(title, content, 'D:\\be-yourself\\data')
+        except Exception as e:
+            print(e)
